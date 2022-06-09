4d6321b649a704467f0b0e1708ea8bb07f6dd145
Comision17 <comision17formar@gmail.com>
2022-06-09 11:13:08 -0300

    Actualizo cambios de la clase 24

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ let path = require('path')
 let app = express()
 let port = 3000
 
-app.get('/', (req,res) => res.send("Bienvenidos a nuestra pagina"))
-app.get('/home',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
+app.use(express.static(path.join(__dirname,'public')))
+
+/* app.get('/', (req,res) => res.send("Bienvenidos a nuestra pagina")) */
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
+
 app.get('/contacto',(req,res) => res.sendFile(path.resolve(__dirname,'views','contacto.html')))
+
+app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')))
+
+app.get('/register',(req,res)=> res.sendFile(path.resolve(__dirname,'views','register.html')))
 
 app.listen(port, () => console.log(`Servidor levantado con exito en http://localhost:${port}`))
 
